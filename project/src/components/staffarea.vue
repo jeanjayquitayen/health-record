be018ab15efedf6b397744cfcd67aba086c78b03
robot-1 <jeanjay.quitayen@asti.dost.gov.ph>
2020-02-10 23:57:38 +0800

    trtyyy

--- project/src/components/staffarea.vue
+++ project/src/components/staffarea.vue
@@ -271,7 +271,7 @@ export default {
 
           if(this.results.v1 == '' && this.v1 != '' && this.v1 != this.vacc[1]) {
             this.results.v1 = this.v1;
-            }else if (this.v1date != null){
+            }else if (this.v1date != null | this.v1date != ''){
               this.results.v1 = this.v1date;
             }else if(this.results.v1 != this.vacc[0]){
               this.results.v1 = this.vacc[1];
@@ -279,14 +279,14 @@ export default {
 
           if(this.results.v2 == '' && this.v2 != '' && this.v2 != this.vacc[1]){
             this.results.v2 = this.v2;
-            }else if (this.v2date != null){
+            }else if (this.v2date != null | this.v2date != ''){
               this.results.v2 = this.v2date;
             }else if(this.results.v2 != this.vacc[0]){
               this.results.v2 = this.vacc[1];
             }
           if(this.results.v3 == '' && this.v3 != '' && this.v3 != this.vacc[1]){
             this.results.v3 = this.v3;
-            }else if (this.results.v3 == this.vacc[1] | this.v3 != '' | this.v3 == this.vacc[1]){
+            }else if (this.v3date != null | this.v3date != ''){
               this.results.v3 = this.v3date;
             }else if(this.results.v3 != this.vacc[0]){
               this.results.v3 = this.vacc[1];
