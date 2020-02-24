180c0b227127d482335769331519c0750f7916ec
jay <robot-1>
2020-02-24 13:36:25 +0800

    change prompt

--- project/src/store/store.js
+++ project/src/store/store.js
@@ -145,7 +145,7 @@ export default new Vuex.Store({
 
           if(data['prc'] == null && this.state.prc == '' && this.state.login == false){
             
-            let pin = prompt("Password:");
+            let pin = prompt("Pin:");
             if(pin == data['pin']){
               commit('setInfo_student',data);
               router.push({path: '/student'});
@@ -156,7 +156,7 @@ export default new Vuex.Store({
 
             
           }else if (this.state.login == false){
-              let pin = prompt("Password:");
+              let pin = prompt("Pin:");
               if(pin == data['pin']){
                 commit('setInfo_staff',data);
                 router.push({path: '/staff'});
